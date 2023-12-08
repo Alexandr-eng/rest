8b10f177a08d39de1187d4c9548f4ed9c3ceafee
Alexandr-eng <122938611+Alexandr-eng@users.noreply.github.com>
2023-12-08 22:57:22 +0500

    Update product_usecases.py

--- eshop/businsess_logic/product_usecases.py
+++ eshop/businsess_logic/product_usecases.py
@@ -2,24 +2,22 @@
 import uuid
 from typing import Optional, List
 
-# from typing import  List
-
 from eshop.businsess_logic.product import Product
 from eshop.data_access.product_repo import get_by_id, get_many, save
 
 
-def product_create(id: str, name: str, price: float):
-    product = (Product(id, name, price))
+def product_create(id: str, name: str, price: float) -> Product:
+    product = Product(id, name, price)
     save(product)
-    return {"success": True, "product": product}
+    return product
 
-def product_get_by_id(id:str) -> Optional[Product]:
+
+def product_get_by_id(id: str) -> Optional[Product]:
     return get_by_id(id)
 
 
 def product_get_many(page: int, limit: int) -> List[Product]:
     return get_many(page=page, limit=limit)
-
 
 
 
